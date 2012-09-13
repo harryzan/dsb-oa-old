317f71543674131e293b6cf06971e4a10a18a772
Zhan Yong Lin <harryzan@gmail.com>
2012-09-13 23:24:35 +0800

    maven project create

---
+++ java/main/oa-sprite/src/main/java/gov/dsb/App.java
@@ -0,0 +1,13 @@
+package gov.dsb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
